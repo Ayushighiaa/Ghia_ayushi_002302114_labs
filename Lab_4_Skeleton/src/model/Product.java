c8d6f032476809face92cdc0793dbf958495d243
Ayushighiaa <ghia.a@northeastern.edu>
2024-10-13 20:26:12 -0400

    commit

---
+++ Lab_4_Skeleton/src/model/Product.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rushabh
+ */
+public class Product {
+    
+    private String name;
+    private int price;
+    private int id;
+    
+   
+
+    private static int count = 0;
+
+    public ArrayList<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(ArrayList<Feature> features) {
+        this.features = features;
+    }
+    private ArrayList<Feature> features;
+    
+    public Product() {
+        count++;
+        id = count;
+        features = new ArrayList<Feature>();    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public Feature addNewFeature() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
